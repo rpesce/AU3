a5cb50cdf33896ad16b469cc4ac746835ebad09e
Roberto <robertopescee@hotmail.com>
2017-03-16 15:31:35 -0700

    Devise models implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable
+  # :lockable, :timeoutable, :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :omniauthable
+         :omniauthable
 end
