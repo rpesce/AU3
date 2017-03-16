2fe75575ec51c4b2f8c3c47af3c31749c09cbdb2
Roberto <robertopescee@hotmail.com>
2017-03-16 13:07:49 -0700

    Installing Devise and OmniAuth

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
+end
