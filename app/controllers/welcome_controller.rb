38a6e1fcf67f62df418f3ec219a8dfb5e3825872
Roberto <robertopescee@hotmail.com>
2017-03-20 11:58:59 -0700

    Implementing articles controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,3 +5,7 @@ before_action :authenticate_user!
   end
 
 end
+
+# This controller should have views for articles, lists and any other important feature
+# in the website. The idea here is to provide a global view of the whole website
+# and access - link - to the specific feature.
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
+  resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
