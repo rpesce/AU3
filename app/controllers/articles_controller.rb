90b69b871cef21a0668ec75583b177d4dfdcae7a
Roberto <robertopescee@hotmail.com>
2017-03-20 17:52:50 -0700

    Implementing Index, Create and Show controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,36 @@
 class ArticlesController < ApplicationController
 
-	def new
+	def index
+		@articles = Article.all
 	end
 
-	def create
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
 	end
 
 	def edit
 	end
 
-	def show
+	def create
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
 	end
 
 	def update
 	end
 
 	def destroy
+	
+	end
+
+private 
+	def article_params
+		params.require(:article).permit(:keywords, :linkSource, :title, :description, :imageLink)
 	end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-before_action :authenticate_user!
+	skip_before_action :authenticate_user!, only: [:index]
 
   def index
   end
